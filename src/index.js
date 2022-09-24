3e71539142eabcc52df4ea07db3189c61f0d8ec5
Danilo Ramalho <daniloti@hotmail.com>
2022-09-23 22:43:59 -0300

    Excluindo .createRoot do index

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 ReactDOM
-  .createRoot(document.getElementById('root'))
   .render(
     <BrowserRouter>
       <App />
     </BrowserRouter>,
+    document.getElementById('root'),
   );
