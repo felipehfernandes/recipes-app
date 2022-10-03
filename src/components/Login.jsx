115afd94f339954b9ddea9621c5fed5f3941ba30
Danilo Ramalho <daniloti@hotmail.com>
2022-10-03 16:04:15 -0300

    Iniciando css

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
+import '../styles/login.css';
+
 const minPasswordLength = 6;
+const URL = 'https://img.freepik.com/fotos-gratis/massa-de-tagliatelle-cru-perto-e-ingredientes-e-molho-de-tomate-sobre-o-plano-de-fundo-texturizado-de-madeira_23-2148195011.jpg?w=2000';
 
 function Login() {
   const [email, setEmail] = useState('');
@@ -32,37 +35,48 @@ function Login() {
   };
 
   return (
-    <form onSubmit={ handleSubmit }>
-      <label htmlFor="email-input">
-        E-mail
-        <input
-          id="email-input"
-          type="email"
-          data-testid="email-input"
-          value={ email }
-          onChange={ ({ target: { value } }) => (handleChange(value, setEmail)) }
+    <main className="mainLogin">
+      <form onSubmit={ handleSubmit } className="formLogin">
+        <img
+          src={ URL }
+          alt="background"
+          className="backgroundImage"
         />
-      </label>
+        <h1>Recipes App</h1>
+        <label htmlFor="email-input" className="labelEmail">
+          E-mail
+          <input
+            className="inputEmail"
+            id="email-input"
+            type="email"
+            data-testid="email-input"
+            value={ email }
+            onChange={ ({ target: { value } }) => (handleChange(value, setEmail)) }
+          />
+        </label>
 
-      <label htmlFor="password-input">
-        Senha
-        <input
-          id="password-input"
-          type="password"
-          data-testid="password-input"
-          value={ password }
-          onChange={ ({ target: { value } }) => (handleChange(value, setPassword)) }
-        />
-      </label>
+        <label htmlFor="password-input" className="labelSenha">
+          Senha
+          <input
+            className="inputEmail"
+            id="password-input"
+            type="password"
+            data-testid="password-input"
+            value={ password }
+            onChange={ ({ target: { value } }) => (handleChange(value, setPassword)) }
+          />
+        </label>
 
-      <button
-        type="submit"
-        data-testid="login-submit-btn"
-        disabled={ !validateLogin() }
-      >
-        Enter
-      </button>
-    </form>
+        <button
+          className="btnSubmitLogin"
+          type="submit"
+          data-testid="login-submit-btn"
+          disabled={ !validateLogin() }
+        >
+          Enter
+        </button>
+      </form>
+    </main>
   );
 }
 
